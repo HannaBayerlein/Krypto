50cbd77571ebf58b4de71207583995b2f8aa66f2
HannaBayerlein <dic14hba@student.lu.se>
2018-11-21 14:01:12 +0100

    Finish

--- Quadratic.java
+++ Quadratic.java
@@ -12,33 +12,81 @@ import java.io.IOException;
 public class Quadratic {
 
     public static void main(String[] args) throws IOException{
-        ArrayList<Integer> primes = eratosthenes(30);
-        BigInteger N = BigInteger.valueOf(16637);
+        ArrayList<Integer> primes = eratosthenes(9000);
+        BigInteger N = new BigInteger("182275519598130020422753");
         ArrayList<BigInteger> rList = new ArrayList<BigInteger>();
-        int[][] binMatrix = rGenerator(rList, primes, N);
-        System.out.println(rList.toString());
-        File inFile = new File("inputMatrix.txt");
-        //File outFile = new File("~/Desktop/outputMatrix.txt");
-        PrintWriter writer = new PrintWriter(inFile, "UTF-8");
-        writer.println("12 10");
-        for(int i = 0; i < binMatrix.length; i++){
-          for(int j = 0; j < binMatrix[0].length; j++){
-            writer.print(binMatrix[i][j] + " ");
+        int L = primes.size() + 24;
+        int[][] binMatrix = rGenerator(rList, primes, N, L);
+        //System.out.println(rList.toString());
+        ArrayList<int[]> solvMatrix = gaussEl(binMatrix);
+        BigInteger factor = findFactor(solvMatrix, rList, N);
+        System.out.println(factor.toString());
+        BigInteger factor2 = N.divide(factor);
+        System.out.println(factor2.toString());
+
+    }
+
+
+    public static BigInteger findFactor(ArrayList<int[]> solvMatrix, ArrayList<BigInteger> rList, BigInteger N){
+      BigInteger one = BigInteger.valueOf(1);
+      BigInteger factor = null;
+      BigInteger x = one;
+      BigInteger y = one;
+      do{
+        int[] solvVec = solvMatrix.get(0);
+        for(int i = 0; i < solvVec.length; i++){
+          if(solvVec[i] == 1){
+            x = x.multiply(rList.get(i));
+            y = y.multiply(rList.get(i).pow(2).mod(N));
           }
-          writer.println();
         }
-        writer.close();
-        ArrayList<String> list = new ArrayList<String>();
-        list.add("/Users/macbook/Desktop/GaussBin.cpp");
-        list.add("/Users/macbook/Desktop/inputMatrix.txt");
-        //list.add("/Users/macbook/Desktop/outputMatrix.txt");
+        x = x.mod(N);
+        y = FloorRoot(y).mod(N);
+        factor = N.gcd(y.subtract(x));
+        solvMatrix.remove(0);
+      }while(factor.equals(one) && !solvMatrix.isEmpty());
 
+      return factor;
+    }
 
-        ProcessBuilder process = new ProcessBuilder(list);
-        process.start();
-        System.out.println("directory: " + process.directory());
+    public static ArrayList<int[]> gaussEl(int[][] binMatrix){
+      ArrayList<int[]> solvMatrix = new ArrayList<int[]>(binMatrix.length);
+      for(int i = 0; i < binMatrix.length; i++){
+        int[] vec = new int[binMatrix.length];
+        vec[i] = 1;
+        solvMatrix.add(vec);
+      }
 
+      ArrayList<int[]> binMatrixList = new ArrayList<int[]>(binMatrix.length);
+      for(int i = 0; i < binMatrix.length; i++){
+        binMatrixList.add(binMatrix[i]);
+      }
+      int oddIndex = Integer.MAX_VALUE;
+      for(int pivot = 0; pivot < binMatrix[0].length; pivot++){
+        for(int row = 0; row < binMatrixList.size(); row++){
+          if(binMatrixList.get(row)[pivot] == 1){
+            if(row < oddIndex){
+              oddIndex = row;
+            } else{
+              for(int col = 0; col < binMatrix[0].length; col++){
+                binMatrixList.get(row)[col] = (binMatrixList.get(row)[col] + binMatrixList.get(oddIndex)[col]) % 2;
+              }
+              for(int col = 0; col < binMatrix[0].length; col++){
+                solvMatrix.get(row)[col] = (solvMatrix.get(row)[col] + solvMatrix.get(oddIndex)[col]) % 2;
+              }
 
+            }
+
+          }
+
+        }
+        if(oddIndex!=Integer.MAX_VALUE){
+          binMatrixList.remove(oddIndex);
+          solvMatrix.remove(oddIndex);
+          oddIndex = Integer.MAX_VALUE;
+        }
+      }
+        return solvMatrix;
     }
 
 
@@ -65,17 +113,17 @@ public class Quadratic {
 
     }
 
-    public static int[][] rGenerator(ArrayList<BigInteger> rList, ArrayList<Integer> primes, BigInteger N){
+    public static int[][] rGenerator(ArrayList<BigInteger> rList, ArrayList<Integer> primes, BigInteger N, int L){
 
-      // 20 är inte rätt!!
-      int[][] binMatrix = new int[12][primes.size()];
+      int[][] binMatrix = new int[L][primes.size()];
       int currentRow = 0;
       BigInteger one = BigInteger.valueOf(1);
       BigInteger two = BigInteger.valueOf(2);
       BigInteger three = BigInteger.valueOf(3);
-      BigInteger twenty = BigInteger.valueOf(15);
-      for(BigInteger k = three; k.compareTo(twenty) < 0; k = k.add(one)){
-        for(BigInteger j = two; j.compareTo(k) <= 0; j = j.add(one)){
+      BigInteger k = one;
+      while(currentRow < binMatrix.length){
+        k = k.add(one);
+        for(BigInteger j = two; j.compareTo(k) <= 0 && currentRow < binMatrix.length; j = j.add(one)){
           BigInteger r = FloorRoot(N.multiply(k)).add(j);
           BigInteger rPow = r.pow(2).mod(N);
           int[] binVec = vecBSmooth(primes, rPow);
@@ -98,9 +146,6 @@ public class Quadratic {
               binMatrix[currentRow][col] = binVec[col];
               }
               currentRow++;
-              if(currentRow == binMatrix.length){
-                return binMatrix;
-              }
             }
           }
         }
@@ -108,9 +153,6 @@ public class Quadratic {
       return binMatrix;
     }
 
-    public static int[][] binaryMatrix(){
-      return null;
-    }
 
     public static int[] vecBSmooth(ArrayList<Integer> primes, BigInteger r){
       BigInteger one = BigInteger.valueOf(1);
